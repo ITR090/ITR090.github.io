86b6f8f1bb975915923bb4c972a7840bca97e6cc
ITR090 <raghad.4309@gmail.com>
2026-06-29 21:50:59 +0300

    git actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Outputs a static 'out' folder instead of requiring a Node server
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // GitHub Pages does not support default Next.js image optimization
+  },
 }
 
 module.exports = nextConfig
